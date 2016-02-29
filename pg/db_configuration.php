b1320102a5108844c5ddcc5880bca5137cb8618b
cmr96 <carlos1m2r3@hotmail.com>
2016-02-29 13:29:58 +0100

    terminado

--- pg/db_configuration.php
+++ pg/db_configuration.php
@@ -5,7 +5,7 @@
    $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
    $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
    $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
-   $db_name="tf"; //Openshift db name
+   $db_name="hardbyte"; //Openshift db name
  } else {
    $db_user="root"; //my db user
    $db_host="localhost"; //my db host
